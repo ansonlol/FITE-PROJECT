86c74a0f6a7e8a13b9ec8582d79da95b7c6ba210
ansonsylee <ansonsylee@gmail.com>
2024-05-10 17:47:19 +0800

    first

--- pages/index.js
+++ pages/index.js
@@ -9,21 +9,48 @@ export default function Home() {
   const [hasMetamask, setHasMetamask] = useState(false);
   const { enableWeb3, isWeb3Enabled } = useMoralis();
 
-  const { data, error, runContractFunction, isFetching, isLoading } =
+  const { data: data1, error: error1, runContractFunction: runMintNFT, isFetching: isfetching1, isLoading: isloading1 } =
     useWeb3Contract({
       abi: abi,
-      contractAddress: "0x5FbDB2315678afecb367f032d93F642f64180aa3", // your contract address here
-      functionName: "store",
+      contractAddress: "0xD99e7a2B5fA64275BE9F3e3EC4e8f12915071cdD", // your contract address on the testnet here
+      functionName: "mintNFT", // function name
+      msgValue: 100,
       params: {
-        _favoriteNumber: 42,
+
       },
     });
+
+  const { data: data2, error: error2, runContractFunction: runREfund, isFetching: isfetching2, isLoading: isloading2 } =
+    useWeb3Contract({
+      abi: abi,
+      contractAddress: "0xD99e7a2B5fA64275BE9F3e3EC4e8f12915071cdD", // your contract address on the testnet here
+      functionName: "refund", // function name
+      // msgValue: 100,
+      params: {
+
+      },
+    });
+
+  const { data: data3, error: error3, runContractFunction: runWithdraw, isFetching: isfetching3, isLoading: isloading3 } =
+    useWeb3Contract({
+      abi: abi,
+      contractAddress: "0xD99e7a2B5fA64275BE9F3e3EC4e8f12915071cdD", // your contract address on the testnet here
+      functionName: "refund", // function name
+      // msgValue: 100,
+      params: {
+
+      },
+    });
+
+
+
 
   useEffect(() => {
     if (typeof window.ethereum !== "undefined") {
       setHasMetamask(true);
     }
   });
+
 
   return (
     <div>
@@ -34,11 +61,29 @@ export default function Home() {
           <button onClick={() => enableWeb3()}>Connect</button>
         )
       ) : (
-        "Please install metamask"
+        "Please install MetaMask"
       )}
-
+      <br />
       {isWeb3Enabled ? (
-        <button onClick={() => runContractFunction()}>Execute</button>
+        <button onClick={() => runMintNFT()}>
+          Mint E-ticket NFT
+        </button>
+      ) : (
+        ""
+      )}
+      <br /><br />
+      {isWeb3Enabled ? (
+        <button onClick={() => runREfund()}>
+          refund
+        </button>
+      ) : (
+        ""
+      )}
+      <br />
+      {isWeb3Enabled ? (
+        <button onClick={() => runWithdraw()}>
+          withdraw
+        </button>
       ) : (
         ""
       )}
